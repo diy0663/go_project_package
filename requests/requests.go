b9fa3c3e4292bb56287dc4ba26639a2ee1168d8e
linhaoxin <779893396@qq.com>
2023-10-21 14:38:08 +0800

    处理传参校验的参数绑定

--- requests/requests.go
+++ requests/requests.go
@@ -15,9 +15,13 @@ type ValidatorFunc func(interface{}, *gin.Context) map[string][]string
 
 // 被API调用
 func ValidateInAPI(c *gin.Context, obj interface{}, handler ValidatorFunc) bool {
-	if err := c.ShouldBindJSON(obj); err != nil {
+	//ShouldBind方法可以处理不同格式的数据，包括JSON、XML、Form表单等，而ShouldBindJSON只能处理JSON格式的数据。
+	//ShouldBind方法会根据请求的Content-Type自动选择绑定器，而ShouldBindJSON只能处理Content-Type为application/json的请求。
+	// 如果严格要求请求的数据格式是JSON，建议使用ShouldBindJSON方法；
+	// 如果请求的数据格式不确定或可能是多种格式，可以使用ShouldBind方法。
+	if err := c.ShouldBind(obj); err != nil {
 		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"message": "请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。",
+			"message": "请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头",
 			"error":   err.Error(),
 		})
 
